5b9e328946c31a18e59bf72b6bc0e85649a41665
leonid.barsucovschi <leonidbarsdev1993@gmail.com>
2023-01-27 15:01:04 +0200

    Add method for sum

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,9 +3,17 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World !!");
+
+        int num1 = 5;
+        int num2 = 56;
+
+        System.out.println( "Sum = " + sum(num1,num2));
 
 
+    }
+
+    public static int sum(int a , int b){
+        return a + b;
     }
 
 }
